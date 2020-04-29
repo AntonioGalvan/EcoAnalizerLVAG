238541d200b0bd1c8a673acfc3a0cfb10e0e00a4
Rodolfo0 <fuzzyrodo@hotmail.com>
2020-04-29 13:31:36 -0500

    Atributos de Advice.cs Se agregó FormAdvice y FormAgregarConsejo con sus respectivos diseños  código

--- presentacion/Data/Advice.cs
+++ presentacion/Data/Advice.cs
@@ -3,8 +3,7 @@
     public class Advice : IEntity
     {
         public int Id { get; set; }
-        public int Numero { get; set; }
-        public string Text { get; set; }
+        public string Texto { get; set; }
         public string Autor { get; set; }
 
         public User User { get; set; }
