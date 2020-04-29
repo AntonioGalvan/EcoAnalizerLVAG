44dd67f2df0413da9978277850f5a261ee46548d
manuel23arredondo <arredondo23manuel@gmail.com>
2020-04-29 18:06:01 -0500

    Forma experto

---
+++ presentacion/FormExperto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace presentacion
+{
+    public partial class frmExperto : MetroFramework.Forms.MetroForm
+    {
+        public frmExperto()
+        {
+            InitializeComponent();
+        }
+
+        private void metroGrid1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void metroButton4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNombre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtApellidos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCiudad_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
